a4aec6af893180efa98c39a0b14476ae6769484f
Sam Spilsbury <smspillaz@gmail.com>
2016-08-23 18:05:29 +0800

    retrieval: Make the retractions directory for the user

--- retrieval.py
+++ retrieval.py
@@ -1,6 +1,8 @@
 import urllib.request
 import contextlib
 import json
+import os
+import errno
 
 searchurl = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=Retracted+Publication&retmax=10&retmode=json"
 fetchurl = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&id="
@@ -11,6 +13,12 @@ with contextlib.closing(urllib.request.urlopen(request)) as byte_response:
 
 data = json.loads(str_response)
 idlist = data["esearchresult"]["idlist"]
+
+try:
+    os.makedirs("Retractions")
+except OSError as error:
+    if error.errno != errno.ENOENT:
+        raise error
 
 for id in idlist:
     downloadurl = "%s%s" % (fetchurl,id)
