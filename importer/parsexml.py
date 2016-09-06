b2940f673a77e0e683f9ea253781ede13936e0f6
Sam Spilsbury <smspillaz@gmail.com>
2016-09-06 18:01:43 +0800

    parsexml: Arrange objects into array and dump valid JSON
    Previously we weren't dumping the array representation which
    made JSON parsing of the process output fail

--- importer/parsexml.py
+++ importer/parsexml.py
@@ -6,6 +6,50 @@ import argparse
 from datetime import date
 
 
+def file_to_element_tree(path):
+    """For a given :path:, get an ElementTree."""
+    return ET.parse(path)
+
+
+def parse_element_tree(tree):
+    """For a given ElementTree :tree:, parse it into JSON."""
+    root = tree.getroot()
+    article_data = {}
+
+    for medinfo in root.iter('MedlineCitation'):
+        article_data['pmid'] = medinfo.find('PMID').text
+
+    for author in root.iter('Author'):
+        lastname = author.find('LastName').text
+        firstname = author.find('ForeName').text
+        authorname = firstname + " " + lastname
+        article_data['Author'] = authorname
+
+    for pubDate in root.iter('DateCompleted'):
+        pubYear = pubDate.find('Year').text
+        pubMonth = pubDate.find('Month').text
+        pubDay = pubDate.find('Day').text
+        pubDateObject = date(int(pubYear), int(pubMonth), int(pubDay))
+        pubDate = pubDateObject.isoformat()
+        article_data['pubDate'] = pubDate
+
+    for reviseDate in root.iter('DateRevised'):
+        reviseYear = reviseDate.find('Year').text
+        reviseMonth = reviseDate.find('Month').text
+        reviseDay = reviseDate.find('Day').text
+        reviseDateObject = date(int(reviseYear), int(reviseMonth), int(reviseDay))
+        reviseDate = reviseDateObject.isoformat()
+        article_data['reviseDate'] = reviseDate
+
+    for journal in root.iter('Journal'):
+        article_data['ISSN'] = journal.find('ISSN').text
+
+    for journalinfo in root.iter('MedlineJournalInfo'):
+        article_data['country'] = journalinfo.find('Country').text
+
+    return article_data
+
+
 def main(argv=None):
     """Parse PubMed XML files in a directory."""
     argv = argv or sys.argv[1:]
@@ -17,45 +61,13 @@ def main(argv=None):
                         metavar="DIR")
     parse_result = parser.parse_args(argv)
 
-    for file in os.listdir(parse_result.directory):
-        if file.endswith(".xml"):
-            tree = ET.parse(os.path.join(parse_result.directory, file))
-            root = tree.getroot()
-            article_data = {}
-            
-            for medinfo in root.iter('MedlineCitation'):
-                article_data['pmid'] = medinfo.find('PMID').text
-                
-            for author in root.iter('Author'):
-                lastname = author.find('LastName').text
-                firstname = author.find('ForeName').text
-                authorname = firstname + " " + lastname
-                article_data['Author'] = authorname
-
-            for pubDate in root.iter('DateCompleted'):
-                pubYear = pubDate.find('Year').text
-                pubMonth = pubDate.find('Month').text
-                pubDay = pubDate.find('Day').text
-                pubDateObject = date(int(pubYear), int(pubMonth), int(pubDay))
-                pubDate = pubDateObject.isoformat()
-                article_data['pubDate'] = pubDate
-
-            for reviseDate in root.iter('DateRevised'):
-                reviseYear = reviseDate.find('Year').text
-                reviseMonth = reviseDate.find('Month').text
-                reviseDay = reviseDate.find('Day').text
-                reviseDateObject = date(int(reviseYear), int(reviseMonth), int(reviseDay))
-                reviseDate = reviseDateObject.isoformat()
-                article_data['reviseDate'] = reviseDate
-
-            for journal in root.iter('Journal'):
-                article_data['ISSN'] = journal.find('ISSN').text
-
-            for journalinfo in root.iter('MedlineJournalInfo'):
-                article_data['country'] = journalinfo.find('Country').text
-
-            json_data = json.dumps(article_data)
-            print(json_data)
+    print(json.dumps([
+        parse_element_tree(
+            file_to_element_tree(os.path.join(parse_result.directory, fn))
+        )
+        for fn in os.listdir(parse_result.directory)
+        if os.path.splitext(fn)[1] == ".xml"
+    ]))
 
 
 if __name__ == "__main__":
