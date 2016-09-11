00c145ba3a35007cbe107dbdf28e776c70f4d4dd
Sam Spilsbury <smspillaz@gmail.com>
2016-09-11 11:07:49 +0800

    app: Use named function expressions

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/", routes);
 app.use("/users", users);
 
 // / catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function handle404(req, res, next) {
   var err = new Error("Not Found");
   err.status = 404;
   next(err);
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
 // will print stacktrace
 
 if (app.get("env") === "development") {
-  app.use(function (err, req, res, next) {
+  app.use(function handleInternalServerErrorDev(err, req, res, next) {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
@@ -60,7 +60,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function handleInternalServerErrorProd(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,
