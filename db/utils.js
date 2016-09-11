f5e3abc56683b5693dbac95dc2dfc38141210b49
Sam Spilsbury <smspillaz@gmail.com>
2016-09-11 11:24:51 +0800

    db/utils: Give functions names

--- db/utils.js
+++ db/utils.js
@@ -1,30 +1,34 @@
+function validateEnvironment (prog) {
+  var requiredVariables = [
+    "DATABASE_URL",
+    "NEO_USER",
+    "NEO_PASS"
+  ];
+
+  requiredVariables.forEach(function (variableName) {
+    if (process.env[variableName] === "undefined") {
+      var msg = variableName + " must be set in the environment before " +
+                        "using this tool. For instance, " + variableName +
+                        "=.... " + String(prog ? prog : "");
+      throw new Error(msg);
+    }
+  });
+
+  return true;
+}
+
+function createConnectionString () {
+  return [
+    "http://",
+    process.env.NEO_USER,
+    ":",
+    process.env.NEO_PASS,
+    "@",
+    process.env.DATABASE_URL
+  ].join("");
+}
+
 module.exports = {
-  validateEnvironment: function (prog) {
-    var requiredVariables = [
-      "DATABASE_URL",
-      "NEO_USER",
-      "NEO_PASS"
-    ];
-
-    requiredVariables.forEach(function (variableName) {
-      if (process.env[variableName] === "undefined") {
-        var msg = variableName + " must be set in the environment before " +
-                          "using this tool. For instance, " + variableName +
-                          "=.... " + String(prog ? prog : "");
-        throw new Error(msg);
-      }
-    });
-
-    return true;
-  },
-  createConnectionString: function () {
-    return [
-      "http://",
-      process.env.NEO_USER,
-      ":",
-      process.env.NEO_PASS,
-      "@",
-      process.env.DATABASE_URL
-    ].join("");
-  }
+  validateEnvironment: validateEnvironment,
+  createConnectionString: createConnectionString
 };
