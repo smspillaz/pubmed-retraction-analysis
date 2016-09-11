a31ee162348b9bf06efd536ad56666e951ae8db1
Sam Spilsbury <smspillaz@gmail.com>
2016-09-11 11:10:13 +0800

    db: Move runAdminInterface into IIFE

--- db/db-admin.js
+++ db/db-admin.js
@@ -3,23 +3,24 @@
 var neo4j = require("node-neo4j");
 var utils = require("./utils");
 
-utils.validateEnvironment("node db-admin.js");
-
-var connectionString = utils.createConnectionString();
-console.log("=> Connecting to " + process.env.DATABASE_URL);
-
-var db = new neo4j(connectionString);
 var stdin = process.openStdin();
 
-stdin.addListener("data", function (d) {
-  console.log("=> Running: " + d.toString().trim());
-  db.cypherQuery(d.toString().trim(), function (err, result) {
-    if (err) {
-      console.log(err);
-      return;
-    }
+utils.validateEnvironment("node db-admin.js");
+console.log("=> Connecting to " + process.env.DATABASE_URL);
 
-    console.log(result.data);
-    console.log(result.columns);
+(function runAdminInterface() {
+  var connectionString = utils.createConnectionString();
+  var db = new neo4j(connectionString);
+  stdin.addListener("data", function (d) {
+    console.log("=> Running: " + d.toString().trim());
+    db.cypherQuery(d.toString().trim(), function (err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      console.log(result.data);
+      console.log(result.columns);
+    });
   });
-});
+}());
