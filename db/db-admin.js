19c8f00802432e097fd538752504d1c42d8e5003
Sam Spilsbury <smspillaz@gmail.com>
2016-09-11 11:10:33 +0800

    db: Make constructor uppercase

--- db/db-admin.js
+++ db/db-admin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var neo4j = require("node-neo4j");
+var Neo4j = require("node-neo4j");
 var utils = require("./utils");
 
 var stdin = process.openStdin();
@@ -10,7 +10,7 @@ console.log("=> Connecting to " + process.env.DATABASE_URL);
 
 (function runAdminInterface() {
   var connectionString = utils.createConnectionString();
-  var db = new neo4j(connectionString);
+  var db = new Neo4j(connectionString);
   stdin.addListener("data", function (d) {
     console.log("=> Running: " + d.toString().trim());
     db.cypherQuery(d.toString().trim(), function (err, result) {
