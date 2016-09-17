6b6f907a5e404806f1838803ca42053033e2be85
Sam Spilsbury <smspillaz@gmail.com>
2016-09-17 15:23:47 +0800

    index: Clean indentation

--- routes/index.js
+++ routes/index.js
@@ -13,48 +13,48 @@ router.get("/", function handleIndexRequest(req, res) {
  * Parameters include 'name', which specifies the name
  * of the chart that we want to get.
  */
-router.get('/get_bar_chart', function(req, res) {
+router.get("/get_bar_chart", function (req, res) {
     // Dummy Values
-    var values = {
-        continentYear: [
-            ["Australia", 5],
-            ["Asia", 5],
-            ["Europe", 10],
-            ["South America", 15],
-            ["North America", 20]
-        ],
-        countryYear: [
-            ["Australia", 10],
-            ["United States", 5],
-            ["France", 15],
-            ["Canada", 30],
-            ["Russian Federation", 10]
-        ],
-        journalYear: [
-            ["Science", 5],
-            ["Nature", 10],
-            ["Pharmacology", 3],
-            ["Radiology", 20],
-            ["Harvard Law Review", 15]
-        ]
-    };
+  var values = {
+    continentYear: [
+      ["Australia", 5],
+      ["Asia", 5],
+      ["Europe", 10],
+      ["South America", 15],
+      ["North America", 20]
+    ],
+    countryYear: [
+      ["Australia", 10],
+      ["United States", 5],
+      ["France", 15],
+      ["Canada", 30],
+      ["Russian Federation", 10]
+    ],
+    journalYear: [
+      ["Science", 5],
+      ["Nature", 10],
+      ["Pharmacology", 3],
+      ["Radiology", 20],
+      ["Harvard Law Review", 15]
+    ]
+  };
 
-    if (Object.keys(values).indexOf(req.query.name) === -1) {
-        res.json({
-            "result": "failure",
-            "reason": "Don't know how to get chart " + req.query.name
-        });
-    } else {
-        res.json({
-            "result": "success",
-            "data": values[req.query.name].map(function(v) {
-                return {
-                    name: v[0],
-                    value: v[1]
-                };
-            })
-        });
-    }
+  if (Object.keys(values).indexOf(req.query.name) === -1) {
+    res.json({
+      "result": "failure",
+      "reason": "Don't know how to get chart " + req.query.name
+    });
+  } else {
+    res.json({
+      "result": "success",
+      "data": values[req.query.name].map(function (v) {
+        return {
+          name: v[0],
+          value: v[1]
+        };
+      })
+    });
+  }
 });
 
 module.exports = router;
