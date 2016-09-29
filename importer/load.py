58bf0e49af838b453abf9fb9b8a365958d2b9fdc
Sam Spilsbury <smspillaz@gmail.com>
2016-09-29 16:15:52 +0800

    load: Add --no-execute flag
    If this flag is passed, then we just print the command instead of
    executing it. We can use this from tests to pass to a testing neo4j
    server and check to see whether we got the result we expected.

--- importer/load.py
+++ importer/load.py
@@ -52,35 +52,38 @@ def generate_command_for_record(record):
 
 def main(argv):
     """Import all data in JSON file into Neo4j database."""
-    if all(var in os.environ for
-           var in ["DATABASE_URL", "DATABASE_USER", "DATABASE_PASS"]):
-                url = os.environ["DATABASE_URL"]
-                pwd = os.environ["DATABASE_PASS"]
-                usr = os.environ["DATABASE_USER"]
-    else:
-        raise ValueError("Ensure environment variables DATABASE_URL, "
-                         "DATABASE_PASS and DATABASE_USER set.")
-
     parser = argparse.ArgumentParser(description="Load articles into Neo4j")
     parser.add_argument("file",
                         help="File to read",
-                        default="data.json",
                         type=str,
                         metavar="FILE")
+    parser.add_argument("--no-execute",
+                        action="store_true")
     parse_result = parser.parse_args(argv)
 
-    driver = GraphDatabase.driver("bolt://" + url, auth=basic_auth(usr, pwd))
-    session = driver.session()
+    with open(parse_result.file or sys.stdin, "r") as fileobj:
+        data = json.load(fileobj)
+        command = "\n".join([a for a in [
+            generate_command_for_record(record)
+            for record in data
+        ] if a is not None])
 
-    with open(parse_result.file, "r") as file:
-        for line in file:
-            data = json.loads(line)
-            for record in data:
-                command = generate_command_for_record(record)
-                if command:
-                    session.run(command)
+    if parse_result.no_execute:
+        print(command)
+    elif command:
+        if all(var in os.environ for
+               var in ["DATABASE_URL", "DATABASE_USER", "DATABASE_PASS"]):
+                    url = os.environ["DATABASE_URL"]
+                    pwd = os.environ["DATABASE_PASS"]
+                    usr = os.environ["DATABASE_USER"]
+        else:
+            raise ValueError("Ensure environment variables DATABASE_URL, "
+                             "DATABASE_PASS and DATABASE_USER set.")
 
-    session.close()
+        driver = GraphDatabase.driver("bolt://" + url, auth=basic_auth(usr, pwd))
+        session = driver.session()
+        session.run(command)
+        session.close()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
