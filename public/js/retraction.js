9811b4e6748072a4469fe0ebcb4dff9d229bf9d7
Sam Spilsbury <smspillaz@gmail.com>
2016-10-06 09:39:08 +0800

    retraction: Deal with labels being too short to fit into the bars

--- public/js/retraction.js
+++ public/js/retraction.js
@@ -6,6 +6,14 @@ var COLOR_TABLE = [
   "#C8D2D9", "#97ABB8", "#FFBEB9", "#FFD1CE"
 ];
 
+/* This is a poor-man's estimate, given the fact that measuring the
+ * actual size of the characters (accounting for kerning) will involve
+ * significant DOM mutation. */
+var INDIVIDUAL_CHARACTER_SIZE = 7;
+
+/**
+ * calculateChartOffset
+
 /**
  * updateGraph
  *
@@ -15,11 +23,41 @@ var COLOR_TABLE = [
  */
 function updateGraph(newData) {
   var svg;
+  var chartContainer = document.getElementsByClassName("chart")[0];
 
   /* The individual points of data, not including labels */
   var dataPoints = newData.map(function onEachDataPoint(w) {
     return w.value;
   });
+
+  /* Estimated sizes of the labels and the bars if no scaling was appleid -
+   * use this to determine whether an offset should be applied to the chart
+   * based on the width of the smallest bar and its corresponding label. If
+   * a bar's label size exceeds the size of the bar, we need to make
+   * at least enough room on the left hand side for the bar label */
+  var offsetAmount = 0;
+  var postprocessedData = newData.map(function onEachDataPoint(d) {
+    var availableParentWidth = chartContainer.clientWidth * 0.80;
+    var x = d3.scaleLinear().domain([0, d3.max(dataPoints)])
+                            .range([0, availableParentWidth]);
+    var bar = x(d.value);
+    var label = 10 + (INDIVIDUAL_CHARACTER_SIZE * d.name.length);
+    var isOffset = false;
+
+    if (label > bar) {
+      isOffset = true;
+      if (offsetAmount < label) {
+        offsetAmount = label;
+      }
+    }
+
+    return {
+      name: d.name,
+      value: d.value,
+      isOffset: isOffset
+    };
+  });
+
 
   /* Drop any existing SVG elements */
   Array.prototype.forEach.call(document.getElementsByClassName("chart"),
@@ -43,14 +81,14 @@ function updateGraph(newData) {
             return ["transform: translate(", x, "px,", y, "px", ")"].join("");
           });
 
-  newData.forEach(function forEachDataPoint(dataPoint, index) {
+  postprocessedData.forEach(function forEachDataPoint(dataPoint, index) {
     var g = svg.append("g")
                .attr("transform", function computeBarY() {
                  var chartHeight = this.parentNode.clientHeight;
                  var barSpacing = 10;
                  var barHeight = ((chartHeight - (barSpacing *
-                                                  newData.length)) /
-                                  newData.length);
+                                                  postprocessedData.length)) /
+                                  postprocessedData.length);
 
                  var y = (index * barSpacing) + (index * barHeight);
                  return "translate(0.0, " + y + ")";
@@ -59,33 +97,35 @@ function updateGraph(newData) {
      .attr("height", function computeBarHeight() {
        var chartHeight = this.parentNode.parentNode.clientHeight;
        var barSpacing = 10;
-       return ((chartHeight - (barSpacing * newData.length)) /
-               newData.length);
+       return ((chartHeight - (barSpacing * postprocessedData.length)) /
+               postprocessedData.length);
      })
      .transition()
      .delay(index * 50)
      .duration(750)
      .ease(d3.easeElastic.period(0.4))
      .attr("width", function computeBarWidth() {
-       var parentWidth = this.parentNode.parentNode.clientWidth;
+       var parentWidth = (this.parentNode.parentNode.clientWidth -
+                          offsetAmount);
        var x = d3.scaleLinear().domain([0, d3.max(dataPoints)])
                                .range([0, parentWidth]);
        return x(dataPoint.value);
      })
      .attr("rx", "15")
      .attr("ry", "15")
+     .attr("x", String(offsetAmount))
      .attr("fill", COLOR_TABLE[index % COLOR_TABLE.length]);
     g.append("text")
-     .attr("fill", "#ffffff")
+     .attr("fill", dataPoint.isOffset ? "#000000" : "#ffffff")
      .text(dataPoint.name)
      .attr("y", function computeY() {
        var chartHeight = this.parentNode.parentNode.clientHeight;
        var barSpacing = 10;
        var textHeight = 15;
-       return (((chartHeight - (barSpacing * newData.length)) /
-               newData.length) / 2) + (textHeight / 2);
+       return (((chartHeight - (barSpacing * postprocessedData.length)) /
+               postprocessedData.length) / 2) + (textHeight / 2);
      })
-     .attr("x", 10);
+     .attr("x", dataPoint.isOffset ? 10 : 10 + offsetAmount);
   });
 }
 
