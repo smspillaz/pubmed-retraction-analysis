075fdd2f1188ad4a92d7112587a73c012e7dd455
Sam Spilsbury <smspillaz@gmail.com>
2016-10-19 21:47:57 +0800

    administration: Make requests to the scraper service

--- routes/administration.js
+++ routes/administration.js
@@ -1,10 +1,53 @@
 var express = require("express");
+var fetch = require("fetch");
 var router = express.Router(); // eslint-disable-line new-cap
 
 /* GET administration page. */
 router.get("/", function handleIndexRequest(req, res) {
   res.render("Administration");
 });
+
+router.get("/is_crawling", function handleIsCrawingRequest(req, res) {
+  if (!process.env.CRAWLING_SERVICE_URL) {
+    res.json({
+      "status": "failure",
+      "msg": "Need to set CRAWLING_SERVICE_URL to use crawler"
+    });
+  } else {
+    fetch.get(process.env.CRAWLING_SERVICE_URL + "/is_crawling").then(function(json) {
+      res.json({
+        "status": "success",
+        "data": json
+      });
+    }).catch(function onError(err) {
+      res.json({
+        "status": "failure",
+        "data": json
+      });
+    });
+  }
+});
+
+router.post("/start_crawling", function handleStartCrawlingRequest(req, res) {
+  if (!process.env.CRAWLING_SERVICE_URL) {
+    res.json({
+      "status": "failure",
+      "msg": "Need to set CRAWLING_SERVICE_URL to use crawler"
+    });
+  } else {
+    fetch.get(process.env.CRAWLING_SERVICE_URL + "/is_crawling").then(function(json) {
+      res.json({
+        "status": "success",
+        "data": json
+      });
+    }).catch(function onError(err) {
+      res.json({
+        "status": "failure",
+        "data": json
+      });
+    });
+  }
+})
 
 
 module.exports = router;
