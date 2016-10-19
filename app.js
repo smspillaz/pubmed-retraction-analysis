7c846795ddcf3865d1f0c2173873427def765a7e
Sam Spilsbury <smspillaz@gmail.com>
2016-10-19 11:40:43 +0800

    app: Use the administration route

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", routes);
 app.use("/visualization", visualization);
+app.use("/administration", visualization);
 
 // / catch 404 and forward to error handler
 app.use(function handle404(req, res, next) {
