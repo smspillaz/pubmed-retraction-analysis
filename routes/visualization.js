463b0a0b1e3fdb08935a2f35ae51b7b27c19a7b6
Sam Spilsbury <smspillaz@gmail.com>
2016-10-20 08:41:00 +0800

    visualization: Move filters to top of file

--- routes/visualization.js
+++ routes/visualization.js
@@ -7,6 +7,13 @@ var db = (function validateEnvironmentAndConnect() {
   return new Neo4j(dbUtils.createConnectionString());
 }());
 
+var filters = {
+  country: "Country",
+  author: "Author",
+  topic: "Topic",
+  year: "Year"
+};
+
 /* GET home page. */
 
 router.get("/", function handleIndexRequest(req, res) {
